b903925a149f2b6f6162d8309591a907c3be1126
EvanNelson04 <nelson01@allegheny.edu>
2025-09-23 14:20:24 -0400

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Evan Nelson on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
